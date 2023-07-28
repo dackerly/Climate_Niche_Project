4e973bd95af2a7c335ccc1027f69fd0aa49bf2ab
dackerly <dackerly@berkeley.edu>
2023-07-28 13:28:28 -0700

    alpha hull analysis for all species

--- scripts/cch-trunc.R
+++ scripts/cch-trunc.R
@@ -43,9 +43,7 @@ cvch <- terra::convHull(cval[,3:4])
 # extract climate for cch
 cch$aet <- extract(aet,cch[,c('longitude','latitude')])[,2]
 cch$cwd <- extract(cwd,cch[,c('longitude','latitude')])[,2]
-vCols <- c(which(names(cch)=='aet'),which(names(cch)=='cwd'))
-
-sp <- 'Sequoia sempervirens'
+vCols <- c(which(names(cch)=='aet'),which(names(cch)=='cwd'))sp <- 'Sequoia sempervirens'
 spr <- which(cch$current_name_binomial==sp)
 #spr <- spr[-153]
 spch <- terra::convHull(cch[spr,c('cwd','aet')])
@@ -73,18 +71,19 @@ kdp <- function(x, # 2-column matrix used to fit kernel density model
 }
 
 # demonstrate use of kernel density function
-cali <- cval[rsamp,3:4]
+cali <- cval[rsamp,]
 redwood <- cch[spr, c("cwd", "aet")]
-1 - mean(kdp(cali, redwood, p = 0.1)) # proportion of redwood occurrences that are outside the 90% CA climate space contour
-plot(cali, pch=19, cex=0.1, xlim=c(0,2000),
-     col=c("lightblue", "dodgerblue")[as.integer(kdp(cali, p = 0.1))+1])
+1 - mean(kdp(cali[,c(3:4)], redwood, p = 0.1)) # proportion of redwood occurrences that are outside the 90% CA climate space contour
+
+head(cali)
+plot(cali[,3:4], pch=19, cex=0.1, xlim=c(0,2000),
+     col=c("lightblue", "dodgerblue")[as.integer(kdp(cali[,3:4], p = 0.1))+1])
 points(redwood, pch=19, cex=0.5, 
-       col=c("darkred", "tomato")[as.integer(kdp(cali, redwood, p = 0.1))+1])
+       col=c("darkred", "tomato")[as.integer(kdp(cali[,3:4], redwood, p = 0.1))+1])
 
 # where are the 10% points?
-plot(cval[rsamp,1:2], pch=19, cex=0.1,
-     col=c("lightblue", "dodgerblue")[as.integer(kdp(cali, p = 0.1))+1])
-
+plot(cali[,1:2], pch=19, cex=0.1,
+     col=c("lightblue", "dodgerblue")[as.integer(kdp(cali[,3:4], p = 0.1))+1])
 
 # function to calculate distance to alpha hull
 alpha_dist <- function(x, # 2-column matrix used to define alpha hull
@@ -111,18 +110,70 @@ alpha_dist <- function(x, # 2-column matrix used to define alpha hull
 }
 
 # demonstrate alpha_dist function with two different radii
-cal <- as.data.frame(cali)
-cal$alpha_0.5 <- alpha_dist(cali, alpha = .5)
-cal$alpha_0.05 <- alpha_dist(cali, alpha = .05)
+cali$alpha_0.5 <- alpha_dist(cali[,3:4], alpha = .5)
+cali$alpha_0.05 <- alpha_dist(cali[,3:4], alpha = .05)
+dim(cali)
+head(cali)
+
+cal <- as.data.frame(cali)  
 cal <- tidyr::gather(cal, alpha, edge_dist, alpha_0.05, alpha_0.5)
-str(cal)
-ggplot(cal, aes(cwd, aet, color = edge_dist)) +
+head(cal)
+hist(cal$edge_dist)
+
+ggplot(cal, aes(cwd, aet, color = 1.5-edge_dist)) +
   facet_wrap(~alpha) +
   geom_point() +
   scale_color_viridis_c()
 
+ggplot(cal, aes(x, y, color = 1.5-edge_dist)) +
+  facet_wrap(~alpha) +
+  geom_point() +
+  scale_color_viridis_c()
+
+hist(cali$alpha_0.05)
+hist(cali$alpha_0.5)
+plot(alpha_0.05~alpha_0.5,data=cali)
+
+rw_dist_0.5 <- alpha_dist(cali[,3:4], redwood, alpha = .5)
+rw_dist_0.05 <- alpha_dist(cali[,3:4], redwood, alpha = .05)
+
+al <- cali$alpha_0.5
+rw <- rw_dist_0.5
+
+head(cali)
+plot(y~x,data=cali,col=c('lightblue','dodgerblue')[cut(al,breaks=c(min(al),quantile(al,0.9),max(al)))],asp=1)
+
+## redwood example was helpful demo. Now do it for all species
+cch2 <- cch[which(complete.cases(cch[,c('cwd','aet')])),]
+cch2$alpha_dist_0.5 <- alpha_dist(cali[,3:4], cch2[,c('cwd','aet')], alpha = .5)
+saveRDS(cch2$alpha_dist_0.5,'data/cch2_alpha_dist_0.5')
+
+allSp <- sort(unique(cch2$current_name_binomial))
+spMar <- data.frame(name=allSp,N=NA,Nm_0.1=NA)
+length(allSp)
+
+spMar$N <- tapply(cch2$current_name_binomial,cch2$current_name_binomial,length)
+
+ltal <- function(x) length(which(x<quantile(al,0.1)))
+spMar$Nm_0.1 <- tapply(cch2$alpha_dist_0.5,cch2$current_name_binomial,ltal)
+
+spMar$Pm_0.1 <- spMar$Nm_0.1/spMar$N
+head(spMar)
+hist(spMar$Pm_0.1)
+dim(spMar)
+
+# number of species that have >= 5% of their observations in the 10% percentile edge of the alpha hull
+length(which(spMar$Pm_0.1>=0.05))
+
+# number of species that have >0 and <5% of their observations in the 10% percentile edge of the alpha hull
+length(which(spMar$Pm_0.1<0.05 & spMar$Pm_0.1>0))
+
+# number of species that have 0% of their observations in the 10% percentile edge of the alpha hull
+length(which(spMar$Pm_0.1==0))
+
 # show that hull can be fit to one data set and distance calculated for a second dataset
-rw_dist <- alpha_dist(cali, redwood, alpha = .5)
+
+str(rw_dist)
 str(cal)
 
 aetAll <- read.csv('results/cchAET.csv')
